c058080a1c2be1a0732e7678466a2399ee0f5022
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-02-18 19:40:20 +0500

    17-20

---
+++ java/index.js
@@ -0,0 +1,83 @@
+Q:1 
+
+// let a = [[]];
+// console.log(a);
+
+Q:2
+
+// let a = [0, 1, 2, 3, 1, 0, 1, 2, 2, 1, 0, 1,]
+
+// document.write(a);
+
+// Q:3
+
+// for (var i = 1; i < 11; i++){
+//     console.log(i)
+// }
+
+Q:4
+
+// let a = +prompt("Enter a Number");
+
+// let b = +prompt("Enter lenght");
+
+// for(let a = 1; a < 10; a++){
+//     document.write('a x' + a + "=" + a * a)
+// }
+
+Q:5
+
+// let arr = ["apple", "banana", "mango", "orange"];
+
+// for(i = 0; i < arr.length; i++){
+//     document.write("<h1> Elemet at index " + i + " is " + arr[i])
+// }
+
+Q:6
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// document.write("<h1>Counting: <br/> </h1>" + a);
+
+// let b = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// document.write("<h1>Reverse Counting: <br/> </h1>" + b);
+
+// let c = [ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+// document.write("<h1>Even: <br/> </h1>" + c);
+
+// let d = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+// document.write("<h1>Odd: <br/> </h1>" + d);
+
+// let e = ["2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k"]
+// document.write("<h1>Series: <br/> </h1>" + e);
+
+
+Q:7;
+// let a = +prompt("What do you want?")
+
+
+// let arr  = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+
+// for(i = 0; i < arr.lenght; i++){
+//     console.log(i + "<h1> is avaliable at index " + [i] +"in our bakery </h1>")
+// }
+
+
+Q:9
+
+// let arr = [12, 55, 86, 110];
+
+// if (arr == arr){
+//     console.log(arr)
+// }
+
+
+let a = ("abc")
+
+document.write(a)
+alert(a)
+console.log(a)
+
+
